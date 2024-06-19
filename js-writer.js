cc635bdcb745c76a6eab121dcf3b4c0c92a8f76f
Ryan Enns <ryanennns@gmail.com>
2024-06-19 09:53:07 -0400

    snickers

--- js-writer.js
+++ js-writer.js
@@ -1,50 +1,50 @@
-import {formatResponse, prompt} from "./ollama.js";
+import {formatResponse, printResponse, prompt} from "./ollama.js";
 import * as test from "./tests/SearchInRotatedSortedArray.js";
 import * as fs from "node:fs";
 
 const responseFormat = {
     javascript: "",
-    reasoning: "",
+    // reasoning: "",
 };
 
 const maxAttempts = 3;
 let attempts = 0;
 
-async function promptLlmForCode(contextAndErrors) {
+const promptLlmForCode = async (contextAndErrors) => {
     let context = contextAndErrors.context ?? [];
     let error = contextAndErrors.error ?? "";
-    let invalidJsonResponse = false;
     let script = "";
-    let promptWithContext = test.default.prompt;
-    let returnedPayload = "";
 
+    let invalidJsonResponse = false;
     do {
         console.log("LLM is generating code...");
         const reader = await prompt(
-            promptWithContext + error,
+            test.default.prompt
+            + "\nHere are the test case results from your previous code:"
+            + JSON.stringify(contextAndErrors.testResults),
             context,
             "You are responsible for writing a JavaScript function based on the prompt provided."
             + "You are to respond in the format provided: " + JSON.stringify(responseFormat) + ". "
             + "the function should be anonymous, and should not contain any console.log statements or \n characters."
-            + "In the reasoning section, you should explain your reasoning for the code you have written and address"
-            + "any changes you have made to fix errors that may have occurred in previous iterations."
+            // + "In the reasoning section, you should write a brief 1-3 sentence reasoning for the code you have written and address"
+            // + "any changes you have made to fix errors that may have occurred in previous iterations."
         );
 
         console.log("Reading response...");
-        const {responseContext, finalString} = await formatResponse(reader);
-        returnedPayload = finalString;
+        const printResponseReturnValue = await formatResponse(reader);
+        let llmJsonPayload = printResponseReturnValue.finalString;
+        context = printResponseReturnValue.context;
 
-        console.log("LLM completed generating code");
         console.log("Sanitizing response...");
 
-        const string = finalString
+        llmJsonPayload = llmJsonPayload
             .replaceAll("\\n", "")
             .replaceAll("\n", "")
             .replaceAll("`", "")
             .replaceAll("\\", "");
 
         try {
-            script = JSON.parse(string).javascript;
+            script = JSON.parse(llmJsonPayload).javascript;
             invalidJsonResponse = false;
         } catch (e) {
             error = e;
@@ -52,10 +52,35 @@ async function promptLlmForCode(contextAndErrors) {
             invalidJsonResponse = true;
         }
     } while (invalidJsonResponse);
-    return {script, returnedPayload};
-}
 
-const generateCodeAndRunTests = async (contextAndErrors = []) => {
+    return {script, context};
+};
+
+const outputTestResultsToFile = (testResults, returnedPrompt) => {
+    fs.appendFile("testResults.json", JSON.stringify({
+        testResults,
+        meta: returnedPrompt,
+    }), (err) => err);
+};
+
+const executeTest = (test, script, expected) => {
+    const actual = test(script);
+
+    if (String(actual) === String(expected)) {
+        process.stdout.write("✓");
+        console.log();
+
+        return {actual, testPassed: true};
+    } else {
+        process.stdout.write(`X`);
+        console.log();
+        console.log(`Expected: ${expected}, received: ${actual}`);
+
+        return {actual, testPassed: false};
+    }
+};
+
+const generateCodeAndRunTests = async (contextAndErrors = {}) => {
     const returnedPrompt = await promptLlmForCode(contextAndErrors);
     const script = returnedPrompt.script;
 
@@ -65,36 +90,23 @@ const generateCodeAndRunTests = async (contextAndErrors = []) => {
     let testResults = [];
     test.default.tests.forEach(({test, expected, testCase}) => {
         process.stdout.write(`Running case: ${testCase}... `);
-        let testPassed = true;
-        const actual = test(script);
+        const testFeedback = executeTest(test, script, expected);
 
-        if (String(actual) === String(expected)) {
-            process.stdout.write("✓");
-            console.log();
-        } else {
-            hasTestFailed = true;
-            testPassed = false;
-            process.stdout.write(`X`);
-            console.log();
-            console.log(`Expected: ${expected}, received: ${actual}`);
-        }
-
-        testResults.push({testCase, expected, actual, testPassed});
+        hasTestFailed = !testFeedback.testPassed;
+        testResults.push({
+            testCase,
+            expected,
+            actual: testFeedback.actual,
+            testPassed: testFeedback.testPassed
+        });
     });
 
-    fs.appendFile("testResults.json", JSON.stringify({
-        testResults,
-        meta: returnedPrompt,
-    }), (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+    outputTestResultsToFile(testResults, returnedPrompt);
 
     if (hasTestFailed && attempts < maxAttempts) {
         console.log("Some tests have failed, retrying...");
         attempts++;
-        await generateCodeAndRunTests({returnedContext, testResults});
+        await generateCodeAndRunTests({testResults, context: returnedPrompt.context});
     } else if (!hasTestFailed) {
         console.log("All tests passed!");
     }
--- tests/harness.js
+++ tests/harness.js
@@ -2,7 +2,5 @@ export const run = (implementation, ...params) => {
     try {
         return eval(`(${implementation})(${params.map(param => JSON.stringify(param)).join(",")})`);
     } catch (e) {
-        // console.log("Error executing function");
-        // console.log(implementation);
     }
 };
